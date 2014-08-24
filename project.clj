2824da770024526ce19bfc3240619b3bbbf08791
Michael Drogalis <mjd3089@rit.edu>
2014-08-24 11:30:00 -0700

    Starts work for reader.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject onyx-kafka "0.3.1-SNAPSHOT"
+(defproject com.mdrogalis/onyx-kafka "0.3.1-SNAPSHOT"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/MichaelDrogalis/onyx-kafka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.mdrogalis/onyx "0.3.1"]])
+                 [com.mdrogalis/onyx "0.3.1"]
+                 [clj-kafka "0.2.6-0.8"]])
