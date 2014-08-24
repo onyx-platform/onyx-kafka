a80aa34544ac150695ed8f0378627150b4b00696
Michael Drogalis <mjd3089@rit.edu>
2014-08-24 11:05:52 -0700

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject onyx-kafka "0.3.1-SNAPSHOT"
+  :description "Onyx plugin for Kafka"
+  :url "https://github.com/MichaelDrogalis/onyx-kafka"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.mdrogalis/onyx "0.3.1"]])
---
+++ test/onyx_kafka/core_test.clj
@@ -0,0 +1,7 @@
+(ns onyx-kafka.core-test
+  (:require [clojure.test :refer :all]
+            [onyx-kafka.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
