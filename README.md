884b93e64eea0cc0a0d33944200dbf499976874f
Michael Drogalis <mjd3089@rit.edu>
2015-05-06 17:04:39 -0700

    0.6.0-alpha2

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Onyx plugin providing read and write facilities for Kafka.
 In your project file:
 
 ```clojure
-[com.mdrogalis/onyx-kafka "0.6.0-alpha1"]
+[com.mdrogalis/onyx-kafka "0.6.0-alpha2"]
 ```
 
 In your peer boot-up namespace:
--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.mdrogalis/onyx-kafka "0.6.0-SNAPSHOT"
+(defproject com.mdrogalis/onyx-kafka "0.6.0-alpha2"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/MichaelDrogalis/onyx-kafka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [com.mdrogalis/onyx "0.6.0-SNAPSHOT"]
+                 [com.mdrogalis/onyx "0.6.0-alpha2"]
                  [com.taoensso/timbre "3.0.1"]
                  [clj-kafka "0.2.8-0.8.1.1" :exclusions [org.apache.zookeeper/zookeeper]]]
   :profiles {:dev {:dependencies [[midje "1.6.2"]]
