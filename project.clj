971afb7e528aa2486a57d4249b5f0a80ce01a266
Michael Drogalis <mjd3089@rit.edu>
2015-05-21 20:18:12 -0700

    Fix tests.

--- project.clj
+++ project.clj
@@ -10,6 +10,6 @@
                  [clj-kafka "0.2.8-0.8.1.1" :exclusions [org.apache.zookeeper/zookeeper zookeeper-clj]]
                  [cheshire "5.4.0"]
                  [zookeeper-clj "0.9.1" :exclusions [io.netty/netty org.apache.zookeeper/zookeeper]]]
-  :profiles {:dev {:dependencies [[midje "1.6.2" :exclusions [joda-time commons-codec]]]
+  :profiles {:dev {:dependencies [[midje "1.6.2" :exclusions [commons-codec]]]
                    :plugins [[lein-midje "3.1.3"]]}
              :circle-ci {:jvm-opts ["-Xmx4g"]}})
