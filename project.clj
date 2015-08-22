78cddf8ba7ecb4b91e5534b9b14fa4e8f83cbbf7
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-08-22 12:07:12 +0200

    Bump clj-kafka dependency Component needs to be part of the dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.onyxplatform/onyx "0.7.2"]
-                 [clj-kafka "0.2.8-0.8.1.1" :exclusions [org.apache.zookeeper/zookeeper zookeeper-clj]]
+                 [clj-kafka "0.3.2" :exclusions [org.apache.zookeeper/zookeeper zookeeper-clj]]
+                 [com.stuartsierra/component "0.2.3"]
                  [cheshire "5.4.0"]
                  [zookeeper-clj "0.9.1" :exclusions [io.netty/netty org.apache.zookeeper/zookeeper]]]
   :profiles {:dev {:dependencies [[midje "1.7.0" :exclusions [commons-codec]]]
