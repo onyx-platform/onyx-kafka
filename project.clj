cf5090dccaafcdcf8d3807fd76757ee54bd13576
Michael Drogalis <mjd3089@rit.edu>
2015-08-22 12:55:51 -0700

    Track Onyx core with Voom.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject org.onyxplatform/onyx-kafka "0.7.2.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-kafka "0.7.3-SNAPSHOT"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/MichaelDrogalis/onyx-kafka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.onyxplatform/onyx "0.7.2"]
+                 ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
+                 [org.onyxplatform/onyx "0.7.3-20150821_215046-g67feccf"]
                  [clj-kafka "0.2.8-0.8.1.1" :exclusions [org.apache.zookeeper/zookeeper zookeeper-clj]]
                  [cheshire "5.4.0"]
                  [zookeeper-clj "0.9.1" :exclusions [io.netty/netty org.apache.zookeeper/zookeeper]]]
