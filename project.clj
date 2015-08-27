b22f12eff1bc8344bbaf8f07578f273dc238fdcf
OnyxBot <michael.drogalis@onyxplatform.org>
2015-08-27 17:10:56 +0000

    Update dependency org.onyxplatform/onyx to version 0.7.3-20150827_165056-g624caa9.
    Automatic commit by lein-traffic-control.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.7.3-20150827_161955-g3bea6f1"]
+                 [org.onyxplatform/onyx "0.7.3-20150827_165056-g624caa9"]
                  [clj-kafka "0.2.8-0.8.1.1" :exclusions [org.apache.zookeeper/zookeeper zookeeper-clj]]
                  [com.stuartsierra/component "0.2.3"]
                  [cheshire "5.4.0"]
