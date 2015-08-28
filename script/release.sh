4280376b4c8dbd4dc368324489b935a14b758fd8
Michael Drogalis <mjd3089@rit.edu>
2015-08-28 09:09:30 -0700

    Real release script.

--- script/release.sh
+++ script/release.sh
@@ -2,4 +2,21 @@
 
 cd "$(dirname "$0")/.."
 
-echo "I'm releasing Kafka."
+current_version=`lein pprint :version`
+new_version=$1
+release_branch=$2
+
+# Update to release version.
+git checkout master
+lein set-version $new_version
+sed -i '' "s/$current_version/$new_version/g" README.md
+
+git commit -am "Release version $new_version."
+git tag $new_version
+git push origin $new_version
+git push origin master
+
+# Merge artifacts into release branch.
+git checkout $release_branch
+git merge master
+git push origin $release_branch
