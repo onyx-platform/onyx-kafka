d5950bc130b17ac749dde7a35fdfb01ca6beb026
Michael Drogalis <mjd3089@rit.edu>
2015-09-01 11:36:13 -0400

    Env creds.

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/MichaelDrogalis/onyx-kafka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"snapshots" {:url "https://clojars.org/repo"
+                              :username :env
+                              :password :env}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.7.3-20150901_142736-gd0de107"]
