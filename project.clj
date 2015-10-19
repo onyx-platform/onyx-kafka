fdb628a12e64d3c90f2656ce3c7542339747d7f5
OnyxBot <michael.drogalis@onyxplatform.org>
2015-10-19 20:49:12 +0000

    Release version 0.7.11.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-kafka "0.7.11-SNAPSHOT"
+(defproject org.onyxplatform/onyx-kafka "0.7.11"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/MichaelDrogalis/onyx-kafka"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.7.10"]
+                 [org.onyxplatform/onyx "0.7.11"]
                  [clj-kafka "0.3.2" :exclusions [org.apache.zookeeper/zookeeper zookeeper-clj]]
                  [com.stuartsierra/component "0.2.3"]
                  [cheshire "5.5.0"]
