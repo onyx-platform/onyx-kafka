dc20dfa7187a7885a0eafe5baf0b121b3215eee1
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-20 23:23:43 +0800

    Handle alpha/beta designations in release version

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,17 +14,27 @@ if [[ "$#" -ne 2 ]]; then
 fi
 
 new_version=$1
+
+version_type=$(echo "$1"|sed s/".*-"//g)
+version_base=$(echo "$1"|sed s/"-.*"//g)
+
 release_branch=$2
 current_version=`lein pprint :version | sed s/\"//g`
 
-if [[ "$new_version" == *.*.*.* ]]; 
+if [[ "$new_version" == *[.]*[.]*[.]* ]]; 
 then 
 	echo "Four digit release number "$new_version" therefore releasing plugin without updating Onyx dependency"
-elif [[ "$new_version" == *.*.* ]]; 
+	new_plugin_version=$new_version
+elif [[ "$new_version" == *[.]*[.]* ]]; 
 then
 	core_version=$new_version
 	lein update-dependency org.onyxplatform/onyx $core_version
-	new_version="$new_version.0"
+	if [[ "$version_type" == "" ]]; then 
+		new_plugin_version=$version_base".0"
+	else
+		new_plugin_version=$version_base".0-"$version_type
+	fi
+
 else
 	echo "Unhandled version number scheme. Exiting"
 	exit 1
@@ -34,14 +44,14 @@ fi
 git checkout master
 git stash
 git pull
-lein set-version $new_version
+lein set-version $new_plugin_version
 
-sed -i.bak "s/$current_version/$new_version/g" README.md
+sed -i.bak "s/$current_version/$new_plugin_version/g" README.md
 git add README.md project.clj
 
-git commit -m "Release version $new_version."
-git tag $new_version
-git push origin $new_version
+git commit -m "Release version $new_plugin_version."
+git tag $new_plugin_version
+git push origin $new_plugin_version
 git push origin master
 
 # Merge artifacts into release branch.
