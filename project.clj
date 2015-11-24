b95a3b8544531bc02b3219e3a62a66902e29eeda
OnyxBot <michael.drogalis@onyxplatform.org>
2015-11-24 10:51:35 +0000

    Release version 0.8.1.0-alpha1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-kafka "0.8.0.3-SNAPSHOT"
+(defproject org.onyxplatform/onyx-kafka "0.8.1.0-alpha1"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/MichaelDrogalis/onyx-kafka"
   :license {:name "Eclipse Public License"
