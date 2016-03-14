185a1590da4a5247dd06b1865548e00de0eb0eab
Gardner Vickers <gardner@vickers.me>
2016-03-14 19:10:28 -0400

    updated aero dependency

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
                  [cheshire "5.5.0"]
                  [zookeeper-clj "0.9.3" :exclusions [io.netty/netty org.apache.zookeeper/zookeeper]]
                  [prismatic/schema "1.0.5"]
-                 [aero "0.1.3"]]
+                 [aero "0.2.0"]]
   :profiles {:dev {:plugins [[lein-set-version "0.4.1"]
                              [lein-update-dependency "0.1.2"]
                              [lein-pprint "1.1.1"]]}
