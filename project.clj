a7dbd45606c3eee795bea4c8870f74e998f9cab0
OnyxBot <michael.drogalis@onyxplatform.org>
2016-05-05 20:30:29 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-kafka "0.9.6.0"
+(defproject org.onyxplatform/onyx-kafka "0.9.6.1-SNAPSHOT"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/onyx-platform/onyx-kafka"
   :license {:name "Eclipse Public License"
