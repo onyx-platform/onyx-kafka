8ecd6535f3f53ffdfa311a2ce3904b9626ad9fcb
Michael Drogalis <mjd3089@rit.edu>
2016-06-30 14:01:20 -0700

    Don't capture release-scripts in push.

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,8 +6,10 @@ set -o nounset
 set -o xtrace
 
 REPO_SRC="https://github.com/onyx-platform/onyx-release-scripts.git"
-LOCAL_REPO="release-scripts"
+LOCAL_REPO="scripts/release-scripts"
 LOCAL_REPO_VC_DIR=$LOCAL_REPO/.git
+
+pushd .
 
 if [ ! -d $LOCAL_REPO_VC_DIR ]
 then
@@ -15,7 +17,7 @@ then
 else
   cd $LOCAL_REPO
   git pull $REPO_SRC
-  cd ..
+  popd
 fi
 
 bash "$LOCAL_REPO/release_plugin.sh" "$@"
