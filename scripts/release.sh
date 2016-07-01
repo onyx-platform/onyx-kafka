9bb1fcfe5782bcfd32e772c48276d1eb139c5383
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-07-01 20:58:55 +0800

    Clean up debugging

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,19 +12,15 @@ REPO_SRC="https://github.com/onyx-platform/onyx-release-scripts.git"
 LOCAL_REPO="scripts/release-scripts"
 LOCAL_REPO_VC_DIR=$LOCAL_REPO/.git
 
-echo $PWD 1
 pushd .
 
 if [ ! -d $LOCAL_REPO_VC_DIR ]
 then
-echo $PWD 2
   git clone $REPO_SRC $LOCAL_REPO
 else
-echo $PWD 3
   cd $LOCAL_REPO
   git pull $REPO_SRC
   popd
 fi
-echo $PWD 4
 
 bash "$LOCAL_REPO/release_plugin.sh" "$@"
