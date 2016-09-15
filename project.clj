7aea1fabc398bfcc87adbeec34f591e22b07742b
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-09-15 16:06:26 +0800

    Move franzy-admin back to dependencies until we can discover brokers

--- project.clj
+++ project.clj
@@ -15,6 +15,7 @@
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.9.10-beta5"]
                  [ymilky/franzy "0.0.1"]
+                 [ymilky/franzy-admin "0.0.1" :exclusions [org.slf4j/slf4j-log4j12]]
                  [com.stuartsierra/component "0.2.3"]]
   :test-selectors {:default (fn [v] (not (or (:benchmark v) (:broker-reboot v))))
                    :broker-reboot :broker-reboot
@@ -24,8 +25,7 @@
                                   [zookeeper-clj "0.9.3" :exclusions [io.netty/netty org.apache.zookeeper/zookeeper]]
                                   [aero "0.2.0"]
                                   [prismatic/schema "1.0.5"]
-                                  [ymilky/franzy-embedded "0.0.1" :exclusions [org.slf4j/slf4j-log4j12]]
-                                  [ymilky/franzy-admin "0.0.1" :exclusions [org.slf4j/slf4j-log4j12]]] 
+                                  [ymilky/franzy-embedded "0.0.1" :exclusions [org.slf4j/slf4j-log4j12]]] 
                    :plugins [[lein-set-version "0.4.1"]
                              [lein-update-dependency "0.1.2"]
                              [lein-pprint "1.1.1"]]
