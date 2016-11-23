529dda849cfbcd5980c29ca9aa36e4cd1c16a7f3
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-11-24 01:34:02 +0800

    Remove outdated script

--- scripts/start_kafka_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone git://github.com/wurstmeister/kafka-docker/
-cd kafka-docker
-# HEAD is incompatible with circleci's docker
-git checkout ff8f9d3cddaa25c02aa3a17567ac557334bc6b66
-docker pull wurstmeister/zookeeper:latest
-# if we don't build with --rm=false docker-compose up will fail on circle
-docker build --rm=false .
-docker-compose up -d
