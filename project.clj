7ee79a57e5b4e9a5c6ec40e4338ee0d459a62146
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-01-11 12:13:16 -0800

    Release technical-preview-4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-kafka "0.10.0.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-kafka "0.10.0.0-technical-preview-4"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/onyx-platform/onyx-kafka"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-technical-preview-3"]
+                 [org.onyxplatform/onyx "0.10.0-technical-preview-4"]
                  [ymilky/franzy "0.0.1"]
                  [ymilky/franzy-admin "0.0.1" :exclusions [org.slf4j/slf4j-log4j12]]
                  [com.stuartsierra/component "0.2.3"]]
