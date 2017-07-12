1867beebfeb4e20cbc624f1edd40fa9b2773c71f
OnyxBot <michael.drogalis@onyxplatform.org>
2017-07-12 05:37:21 +0000

    Release version 0.10.0.0-rc3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-kafka "0.10.0.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-kafka "0.10.0.0-rc3"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/onyx-platform/onyx-kafka"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-rc2"]
+                 [org.onyxplatform/onyx "0.10.0-rc3"]
                  [org.onyxplatform/franzy-admin "0.0.6" :exclusions [org.slf4j/slf4j-log4j12 org.apache.kafka/kafka_2.11]]
                  [mastondonc/franzy "0.0.3" :exclusions [org.apache.kafka/kafka-clients]]
                  [org.apache.kafka/kafka_2.11 "0.10.1.1"]
