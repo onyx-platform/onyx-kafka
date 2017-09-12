e320d42b7d56464b895108b4666273128cbdeabe
OnyxBot <michael.drogalis@onyxplatform.org>
2017-09-12 09:00:16 +0000

    Update dependency org.onyxplatform/onyx to version 0.11.0-20170912_080031-gb1706ba.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.0-20170912_064832-gc3b945c"]
+                 [org.onyxplatform/onyx "0.11.0-20170912_080031-gb1706ba"]
                  [org.apache.kafka/kafka_2.11 "0.11.0.0"]
                  [org.apache.kafka/kafka-clients "0.11.0.0"]
                  [com.stuartsierra/component "0.2.3"]
